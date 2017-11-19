f345402a2dcda15d2aef47bfb8fff54397898ea5
monelife <liguoqiang2001@sina.com>
2017-11-19 23:25:57 +0800

    修改drupalphp-cmd.sh命令的执行顺序

--- drupalphp-cmd.sh
+++ drupalphp-cmd.sh
@@ -8,20 +8,19 @@ if [ ! -f ${DRUPAL_ROOT}/sites/default/settings.php ]; then
     mv /usr/src/drupal8/drupal.tar.gz ${DRUPAL_ROOT}
     cd ${DRUPAL_ROOT}
     tar -xz --strip-components=1 -f drupal.tar.gz
-    rm drupal.tar.gz
+    rm drupal.tar.gz    
+  else
+    echo "下载drupal安装包出现问题, 尝试重新运行 docker-compose up -d 命令"  
+  fi
     
+else 
+  echo "drupal 8 已经安装, 如需升级请使用drush"
+fi
+
 # 在drupal根目录安装 drupalconsole本身      
     composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader --sort-packages
     
     chown -R www-data:www-data ${DRUPAL_ROOT}
     chown -R www-data:www-data ${DRUPAL_PRIVATE}
     
-  else
-    echo "下载drupal安装包出现问题, 尝试重新运行 docker-compose up -d 命令"  
-  fi
-  
-else 
-  echo "drupal 8 已经安装, 如需升级请使用drush"
-fi
-
 exec php-fpm
