ff81a7183b917e26d29013702f39f9405f042d2c
monelgq <liguoqiang2001@sina.com>
2020-07-02 01:40:49 +0800

    更新 if 格式

--- drupalphp-cmd.sh
+++ drupalphp-cmd.sh
@@ -5,7 +5,7 @@ set -e
 cd ${DRUPAL_ROOT}
 
 # 判断站点是否已安装或者站点私有目录中是否存在升级提示文件 drupal.update
-if [ ! -f ${DRUPAL_ROOT}/sites/default/settings.php ] || [ -f /var/www/private/drupal.update]; then
+if [ ! -f ${DRUPAL_ROOT}/sites/default/settings.php ] || [ -f /var/www/private/drupal.update ]; then
 
   # 下载指定网址的 drupal8或drupal9最新版本在drupal站点根目录【全新安装或者覆盖升级】
   curl -fSL ${DRUPAL_SRC} -o drupal.tar.gz
@@ -15,7 +15,7 @@ if [ ! -f ${DRUPAL_ROOT}/sites/default/settings.php ] || [ -f /var/www/private/d
     rm drupal.tar.gz
   
     # 在drupal根目录安装 drupal console 本身
-    #composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader
+    # composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader
     
   else
     echo "下载drupal源码安装包出现问题, 尝试重新运行 docker-compose up -d 命令"
